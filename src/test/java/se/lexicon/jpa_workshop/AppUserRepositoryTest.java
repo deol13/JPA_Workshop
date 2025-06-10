a6685dd474fb4ebd16ae3c72ec5c9fcb6aaf141e
Dennis Olsen <dennan_lol@hotmail.com>
2025-06-10 12:16:10 +0200

    Part 2 testing

---
+++ src/test/java/se/lexicon/jpa_workshop/AppUserRepositoryTest.java
@@ -0,0 +1,135 @@
+package se.lexicon.jpa_workshop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import se.lexicon.jpa_workshop.entity.AppUser;
+import se.lexicon.jpa_workshop.entity.Details;
+import se.lexicon.jpa_workshop.repository.AppUserRepository;
+import se.lexicon.jpa_workshop.repository.DetailsRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest // Will set up and configure a test database we will use during the tests
+/*
+configuring H2, an in-memory database
+setting Hibernate, Spring Data, and the DataSource
+performing an @EntityScan
+turning on SQL logging
+ */
+public class AppUserRepositoryTest {
+    @Autowired // Dependency Inject field
+    AppUserRepository appUserRepository;
+    @Autowired // Dependency Inject field
+    DetailsRepository detailsRepository;
+
+    @Test
+    void whenSaveAppUser_thenCorrect() {
+        // given
+        AppUser appUser = new AppUser("un","123456", LocalDate.now());
+
+        // when
+        AppUser createdUser = appUserRepository.save(appUser);
+
+        // then
+        Assertions.assertNotNull(createdUser);
+        Assertions.assertEquals(createdUser.getId(), appUser.getId());
+    }
+
+    @Test
+    void whenDeleteAppUser_thenCorrect() {
+        // given
+        AppUser appUser = new AppUser("un","123456", LocalDate.now());
+
+        // when
+        appUserRepository.delete(appUser);
+        AppUser deletedUser = appUserRepository.findById(appUser.getId()).orElse(null);
+
+        // then
+        Assertions.assertNull(deletedUser);
+    }
+
+    @Test
+    void whenUpdateAppUser_thenCorrect() {
+        // given
+        AppUser appUser = new AppUser("un","123456", LocalDate.now());
+        AppUser createdUser = appUserRepository.save(appUser);
+
+        // when
+        appUser.setUsername("update");
+        AppUser updatedUser = appUserRepository.save(createdUser);
+
+        // then
+        Assertions.assertNotNull(updatedUser);
+        Assertions.assertEquals(updatedUser.getId(), createdUser.getId());
+        Assertions.assertEquals("update", updatedUser.getUsername());
+    }
+
+    @Test
+    void whenFindAppUsersById_thenCorrect() {
+        //given
+        AppUser appUser = new AppUser("un","123456", LocalDate.now());
+        AppUser createdUser = appUserRepository.save(appUser);
+
+        // when
+        Optional<AppUser> foundAppUser = appUserRepository.findById(createdUser.getId());
+
+        // then
+        Assertions.assertTrue(foundAppUser.isPresent());
+        Assertions.assertEquals(createdUser.getId(), foundAppUser.get().getId());
+    }
+
+    @Test
+    void whenFindByUsername_thenCorrect() {
+        // given
+        AppUser appUser = new AppUser("un","123456", LocalDate.now());
+        AppUser appUser2 = new AppUser("un2","1234567", LocalDate.now());
+        AppUser createdUser = appUserRepository.save(appUser);
+        appUserRepository.save(appUser2);
+
+        // when
+        List<AppUser> foundAppUser = appUserRepository.findByUsername(createdUser.getUsername());
+
+        // then
+        Assertions.assertFalse(foundAppUser.isEmpty());
+        Assertions.assertEquals(1, foundAppUser.size());
+        Assertions.assertEquals(foundAppUser.get(0), createdUser);
+    }
+
+    @Test
+    void whenFindByRegDateBetween_thenCorrect() {
+        // given
+        AppUser appUser = new AppUser("un","123456", LocalDate.of(2020,1,1));
+        AppUser appUser2 = new AppUser("un2","1234567", LocalDate.now());
+        AppUser createdUser = appUserRepository.save(appUser);
+        AppUser createdUser2 = appUserRepository.save(appUser2);
+
+        // when
+        List<AppUser> foundAppUser = appUserRepository.findByRegDateBetween(LocalDate.of(2019,1,1), LocalDate.of(2021,1,1));
+
+        // then
+        Assertions.assertFalse(foundAppUser.isEmpty());
+        Assertions.assertEquals(1, foundAppUser.size());
+        Assertions.assertEquals(foundAppUser.get(0), createdUser);
+    }
+
+    @Test
+    void whenFindByDetailsId_thenCorrect() {
+        // given
+        Details details = new Details("testName","testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        AppUser appUser = new AppUser("un","123456", LocalDate.now(), createdDetails);
+        AppUser createdUser = appUserRepository.save(appUser);
+
+        // when
+        AppUser foundAppUser = appUserRepository.findByDetailsId(createdUser.getUserDetails().getId());
+
+        // then
+        Assertions.assertNotNull(foundAppUser);
+        Assertions.assertEquals(createdUser.getUserDetails().getId(), foundAppUser.getUserDetails().getId());
+    }
+}
---
+++ src/test/java/se/lexicon/jpa_workshop/DetailsRepositoryIntegrationTest.java
@@ -0,0 +1,137 @@
+package se.lexicon.jpa_workshop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import se.lexicon.jpa_workshop.entity.AppUser;
+import se.lexicon.jpa_workshop.entity.Details;
+import se.lexicon.jpa_workshop.repository.DetailsRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+public class DetailsRepositoryIntegrationTest {
+
+    @Autowired // Dependency Inject field
+    DetailsRepository detailsRepository;
+
+    @Test
+    void whenSaveDetail_thenCorrect() {
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+
+        // when
+        Details createdDetails = detailsRepository.save(details);
+
+        // then
+        Assertions.assertNotNull(createdDetails);
+        Assertions.assertEquals(createdDetails.getId(), details.getId());
+    }
+
+    @Test
+    void whenDeleteDetail_thenCorrect() {
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+
+        // when
+        detailsRepository.delete(details);
+        Details deletedDetails = detailsRepository.findById(details.getId()).orElse(null);
+
+        // then
+        Assertions.assertNull(deletedDetails);
+    }
+
+    @Test
+    void whenUpdateDetail_thenCorrect() {
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        details.setName("updateName");
+        Details updatedDetails = detailsRepository.save(createdDetails);
+
+        // then
+        Assertions.assertNotNull(updatedDetails);
+        Assertions.assertEquals(updatedDetails.getId(), createdDetails.getId());
+        Assertions.assertEquals("updateName", updatedDetails.getName());
+    }
+
+    @Test
+    void whenFindDetailsById_thenCorrect() {
+        //given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        Optional<Details> foundDetails = detailsRepository.findById(createdDetails.getId());
+
+        // then
+        Assertions.assertTrue(foundDetails.isPresent());
+        Assertions.assertEquals(createdDetails.getId(), foundDetails.get().getId());
+    }
+
+
+    @Test
+    void whenFindByEmail_thenCorrect(){
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        List<Details> foundDetails = detailsRepository.findByEmail(createdDetails.getEmail());
+
+        // then
+        Assertions.assertFalse(foundDetails.isEmpty());
+        Assertions.assertEquals(1, foundDetails.size());
+        Assertions.assertEquals(foundDetails.get(0), createdDetails);
+    }
+
+    @Test
+    void whenFindByEmailIgnoreCase_thenCorrect(){
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        List<Details> foundDetails = detailsRepository.findByEmailIgnoreCase(createdDetails.getEmail().toLowerCase());
+
+        // then
+        Assertions.assertFalse(foundDetails.isEmpty());
+        Assertions.assertEquals(1, foundDetails.size());
+        Assertions.assertEquals(foundDetails.get(0), createdDetails);
+    }
+
+    @Test
+    void whenFindByNameContaining_thenCorrect(){
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        List<Details> foundDetails = detailsRepository.findByNameContaining("Name");
+
+        // then
+        Assertions.assertFalse(foundDetails.isEmpty());
+        Assertions.assertEquals(1, foundDetails.size());
+        Assertions.assertEquals(foundDetails.get(0), createdDetails);
+    }
+
+    @Test
+    void whenFindByNameIgnoreCase_thenCorrect(){
+        // given
+        Details details = new Details("testName", "testEmail", LocalDate.now());
+        Details createdDetails = detailsRepository.save(details);
+
+        // when
+        List<Details> foundDetails = detailsRepository.findByNameIgnoreCase(createdDetails.getName().toLowerCase());
+
+        // then
+        Assertions.assertFalse(foundDetails.isEmpty());
+        Assertions.assertEquals(1, foundDetails.size());
+        Assertions.assertEquals(foundDetails.get(0), createdDetails);
+    }
+}
