29fbee7b28f64c1c60b0e0d7fd39efc0b5b8a848
Dennis Olsen <dennan_lol@hotmail.com>
2025-06-17 16:52:47 +0200

    Testing the relationship between AppUser and BookLoan
    # Conflicts:
    #	src/main/java/se/lexicon/jpa_workshop/MyCommandLineRunner.java
    #	src/main/java/se/lexicon/jpa_workshop/entity/AppUser.java

--- src/main/java/se/lexicon/jpa_workshop/MyCommandLineRunner.java
+++ src/main/java/se/lexicon/jpa_workshop/MyCommandLineRunner.java
@@ -30,35 +30,78 @@ public class MyCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("###### Application started successfully ######");
+        //appUserAndDetails();
+        //bookAndAuthor();
+        bookLoan();
+    }
 
+
+    public void appUserAndDetails() {
+        System.out.println("###### App user and details ######");
         Details details = new Details("John Doe", "JohnDoe@email.com", LocalDate.of(1990, 1, 1));
-//        Details createdDetails = detailsRepository.save(details);
-//        System.out.printf("Details created: %s\n", createdDetails);
 
-        AppUser user = new AppUser("admin", "123", details);
+        AppUser user = new AppUser("admin", "123", LocalDate.of(1995, 1, 1), details);
         AppUser createdAppUser = appUserRepository.save(user);
-        AppUser test = appUserRepository.findById(createdAppUser.getId()).orElse(null);
-        System.out.printf("AppUser created: %s\n", test);
+        System.out.printf("\nAppUser created: %s\n", createdAppUser);
+    }
+
+    public void bookAndAuthor() {
+        System.out.println("\n###### Book and Author ######");
+
+        Book book = new Book("Lord of the Ring", 35);
+        Book createdBook2 = bookRepository.save(book);
+        System.out.printf("\nBook created: %s\n", createdBook2);
+
+        Author author = new Author("Dennis", "Olsen", LocalDate.of(1995, 1, 1));
+        author.addWrittenBook(book);
+        Author createdAuthor = authorRepository.save(author);
+        System.out.printf("\nAuthor created: %s\n", createdAuthor);
+
+        int id2 = authorRepository.updateAuthorNameById("Johan", "Olsen", createdAuthor.getId());
+        Author updatedAuthor = authorRepository.findById((long) id2).orElse(null);
+        System.out.printf("\nAuthor updated: %s\n", updatedAuthor);
+
+        createdAuthor.removeWrittenBook(book);
+        Author updatedAuthor2 = authorRepository.save(updatedAuthor);
+        System.out.printf("\nBook removed from Author: %s\n", updatedAuthor2);
+    }
+
+    public void bookLoan() {
+        System.out.println("\n###### Book loan ######");
+
+        Details details = new Details("Johnny Doe", "JohnnyDoe@email.com", LocalDate.of(1990, 1, 1));
+
+        AppUser user = new AppUser("admin2", "123", LocalDate.of(1995, 1, 1), details);
+        AppUser createdAppUser = appUserRepository.save(user);
+        System.out.printf("\nAppUser created: %s\n", createdAppUser);
+
 
         Book book = new Book("The Hobbit", 30);
         Book createdBook = bookRepository.save(book);
         System.out.printf("\nBook created: %s\n", createdBook);
 
-//        BookLoan bookLoan = new BookLoan(LocalDate.of(2025, 7, 1), createdAppUser, createdBook);
-//        BookLoan createdBookLoan = bookLoanRepository.save(bookLoan);
-//        System.out.printf("\nBook loan created: %s\n", createdBookLoan);
+        BookLoan bookLoan = new BookLoan(LocalDate.of(2025, 7, 1), createdBook);
+        //BookLoan bookLoan2 = new BookLoan(LocalDate.of(2025, 7, 1), createdBook, createdAppUser);
+        BookLoan createdBookLoan = bookLoanRepository.save(bookLoan);
 
-//        int id = bookLoanRepository.updateBookLoanReturnedTrueById(createdBookLoan.getId());
-//        BookLoan updatedBookLoan = bookLoanRepository.findById((long) id).orElse(null);
-//        System.out.printf("\nBook loan updated: %s\n", updatedBookLoan);
+        createdAppUser.addBookLoan(bookLoan);
+        AppUser updatedAppUser = appUserRepository.save(createdAppUser);
+        System.out.printf("\nAppUser updated with book loan: %s\n", updatedAppUser);
 
-        Author author = new Author("John", "Doe", LocalDate.of(1995, 1, 1));
-        author.addWrittenBook(book);
-        Author createdAuthor = authorRepository.save(author);
-        System.out.printf("\nAuthor created: %s\n", createdAuthor);
+        System.out.printf("\nBookLoan list from App user: %s\n", createdAppUser.getBookLoanList());
 
-        int id2 = authorRepository.updateAuthorNameById("Dennis", "Olsen", createdAuthor.getId());
-        Author updatedAuthor = authorRepository.findById((long) id2).orElse(null);
-        System.out.printf("\nAuthor updated: %s\n", updatedAuthor);
+        int id = bookLoanRepository.updateBookLoanReturnedTrueById(createdBookLoan.getId());
+        BookLoan updatedBookLoan2 = bookLoanRepository.findById((long) id).orElse(null);
+        AppUser updatedAppUser2 = appUserRepository.findById(updatedAppUser.getId()).orElse(null); // todo: gives LazyInitializationException
+        System.out.printf("\nBook loan updated to returned: %s\n", updatedBookLoan2);
+
+        // todo: test if remove book loan updates book loan as well or if I have to manually save it
+        updatedAppUser2.removeBookLoan(updatedBookLoan2);
+        AppUser updatedAppUser3 = appUserRepository.save(updatedAppUser);
+        System.out.printf("\nAppUser removed book loan: %s\n", updatedAppUser2);
+
+        //BookLoan createdBookLoan2 = bookLoanRepository.save(bookLoan);
+        //System.out.printf("\nBook loan: %s\n", updatedBookLoan2);
     }
+
 }
