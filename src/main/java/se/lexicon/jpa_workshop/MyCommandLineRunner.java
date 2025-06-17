872916aaeeedcbf8845b6183ffba621d559ea078
Dennis Olsen <dennan_lol@hotmail.com>
2025-06-17 10:01:34 +0200

    Added casecadeType to Appuser and some more @column restrictions.

--- src/main/java/se/lexicon/jpa_workshop/MyCommandLineRunner.java
+++ src/main/java/se/lexicon/jpa_workshop/MyCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import se.lexicon.jpa_workshop.entity.AppUser;
+import se.lexicon.jpa_workshop.entity.Book;
+import se.lexicon.jpa_workshop.entity.BookLoan;
 import se.lexicon.jpa_workshop.entity.Details;
 import se.lexicon.jpa_workshop.repository.AppUserRepository;
 import se.lexicon.jpa_workshop.repository.BookLoanRepository;
@@ -32,12 +34,20 @@ public class MyCommandLineRunner implements CommandLineRunner {
         System.out.println("###### Application started successfully ######");
 
         Details details = new Details("John Doe", "JohnDoe@email.com", LocalDate.of(1990, 1, 1));
-        Details createdDetails = detailsRepository.save(details);
-        System.out.printf("Details created: %s\n", createdDetails);
+        //Details createdDetails = detailsRepository.save(details);
+        //System.out.printf("Details created: %s\n", createdDetails);
 
-        AppUser user = new AppUser("admin", "123", LocalDate.of(1995, 1, 1), createdDetails);
+        AppUser user = new AppUser("admin", "123", LocalDate.of(1995, 1, 1), details);
         AppUser createdAppUser = appUserRepository.save(user);
         System.out.printf("AppUser created: %s\n", createdAppUser);
+
+        Book book = new Book("The Hobbit", 30);
+        Book createdBook = bookRepository.save(book);
+        System.out.printf("\nBook created: %s\n", createdBook);
+
+        BookLoan bookLoan = new BookLoan(LocalDate.of(2025, 7, 1), createdAppUser, createdBook);
+        BookLoan createdBookLoan = bookLoanRepository.save(bookLoan);
+        System.out.printf("\nBook loan created: %s\n", createdBookLoan);
 
     }
 }
