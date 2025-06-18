76e191a9ecc27d3abb4ccd1e7773afe0710a84b2
Dennis Olsen <dennan_lol@hotmail.com>
2025-06-18 13:22:55 +0200

    Fixed ManyToMany between Author and Book. Updated some @Columns

--- src/main/java/se/lexicon/jpa_workshop/entity/Book.java
+++ src/main/java/se/lexicon/jpa_workshop/entity/Book.java
@@ -29,20 +29,22 @@ public class Book {
     private String title;
     private int maxLoadDays;
 
-//    @ManyToMany(mappedBy = "writtenBooks", fetch = FetchType.EAGER)
-//    private Set<Author> authors = new HashSet<>();
+    @ManyToMany(mappedBy = "writtenBooks")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Set<Author> authors = new HashSet<>();
 
     public Book(String title, int maxLoadDays) {
         this.title = title;
         this.maxLoadDays = maxLoadDays;
     }
 
-//    public void addAuthor(Author author) {
-//        this.authors.add(author);
-//        author.getWrittenBooks().add(this);
-//    }
-//    public void removeAuthor(Author author) {
-//        this.authors.remove(author);
-//        author.getWrittenBooks().remove(this);
-//    }
+    public void addAuthor(Author author) {
+        this.authors.add(author);
+        author.getWrittenBooks().add(this);
+    }
+    public void removeAuthor(Author author) {
+        this.authors.remove(author);
+        author.getWrittenBooks().remove(this);
+    }
 }
